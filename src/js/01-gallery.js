6124bdaa0540cd3fa21e74fe562f79fd832b0eda
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-04-08 01:08:24 +0300

    Первое задание готово

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const listGallery = createGallery(galleryItems);
+
+galleryContainer.innerHTML = listGallery;
+
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => `
+<li>
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}"
+  alt="${description}" />
+</a>
+</li>`).join('');
+    
+}
+
+let gallery = new SimpleLightbox('.gallery a',
+         {
+             navText: ['<', '>'],
+             captionsData: 'alt',    
+             captionPosition: 'bottom',
+             captionDelay: 250,
+
+         });
+    
+    console.log(gallery);
