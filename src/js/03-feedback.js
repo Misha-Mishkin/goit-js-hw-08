9c3df7f786eeaa2903fb367e71dff18897ab369d
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-04-10 21:48:58 +0300

    Третье задание решено

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const KEY = "feedback-form-state";
+
+const formData = {};
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener('input', throttle(onTextAreaInput, 500));
+form.addEventListener('submit', onButtonClick);
+
+updateForm();
+
+function onTextAreaInput(evt) {
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function onButtonClick(evt) {
+    evt.preventDefault();
+
+    const formDataToSend = new FormData(evt.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(KEY);
+}
+
+function updateForm() {
+    const savedMessage = localStorage.getItem(KEY);
+
+    if (savedMessage) {
+        const { email, message } = JSON.parse(savedData);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
+    }
+};
